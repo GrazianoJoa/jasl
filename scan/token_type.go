3937c6112dcf67dfb69269bc59da883a462aaee3
joaquin <grazianojoaquin0@gmail.com>
2026-04-04 03:31:06 -0300

    init

---
+++ scan/token_type.go
@@ -0,0 +1,46 @@
+const (
+  LeftParen = iota
+  RightParen
+  LeftBrace
+  RightBrace
+
+  Plus
+  Minus
+  Star
+  Slash
+
+  Dot
+  Comma
+  Semicolon
+
+	Equal
+	EqualEqual
+	Bang
+	BangEqual
+	Less
+	LessEqual
+	Right 
+	RightEqual
+
+	Identifier
+	String
+	Number
+
+	If 
+	Else 
+	True
+	False
+	Nil
+
+	Var
+	For
+	While
+
+	Fun 
+	Return
+	Print
+
+	Class
+	Super
+	This
+)
